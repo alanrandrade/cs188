6ede8c79bc4d1b2a4d52e47eb0af464af6b8c5a8
Jesper van de Putte <jespervandeputte@gmail.com>
2018-05-18 14:17:47 +0200

    new basis of objects

--- p3/extract_ghost_movements_percentage.py
+++ p3/extract_ghost_movements_percentage.py
@@ -9,24 +9,46 @@ import numpy as np
 
 NUMBER_OF_GHOSTS = 4;
 NUMBER_OF_GAMES = 201;
+TRAINING_SESSIONS = 99;
+FIELD_OF_VIEW = 10;
+POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD = 4;
 
 filename = "ghost_movements.csv"
 file = open(filename, "r")
 previous = ""
 
+# leftG0 = 0
+# rightG0 = 0
+# upG0 = 0
+# downG0 = 0
+# leftG1 = 0
+# rightG1 = 0
+# upG1 = 0
+# downG1 = 0
+# leftG2 = 0
+# rightG2 = 0
+# upG2 = 0
+# downG2 = 0
+# leftG3 = 0
+# rightG3 = 0
+# upG3 = 0
+# downG3 = 0
 
 ArrayWithGhostCounters = np.zeros(shape = (NUMBER_OF_GHOSTS,4))
 
-PercentagesOfGames = np.zeros(shape=(NUMBER_OF_GAMES, ArrayWithGhostCounters.shape[0], ArrayWithGhostCounters.shape[1]))
-upPercentage =downPercentage = leftPercentage = rightPercentage = np.zeros(shape = (4 ,NUMBER_OF_GAMES), dtype =float)
+NEWArrayWithGhostCounters = np.zeros(shape=(FIELD_OF_VIEW*2,FIELD_OF_VIEW*2, NUMBER_OF_GHOSTS,POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD))
+
+PercentagesOfGames = np.zeros(shape=(NUMBER_OF_GAMES - TRAINING_SESSIONS, FIELD_OF_VIEW*2,FIELD_OF_VIEW*2, NUMBER_OF_GHOSTS,POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD))
 
 counter = 0
+training_counter = 0;
 
-
+print(NEWArrayWithGhostCounters.shape)
+print(PercentagesOfGames.shape)
 
 
 for line in file:
-	if(line != previous):
+    if(line != previous):
 
 		# if(line == "End of game"):
 		# 	/float(upG0 + downG0 + leftG0 + rightG0)
@@ -35,74 +57,66 @@ for line in file:
 		# 	/float(upG3 + downG3 + leftG3 + rightG3)
 
 
-		m = re.findall(r'\d+(?:,\d+)?', line)#re.match(r'PacmanPos, (.*), (.*), Ghost0Pos (.*), (.*), Ghost1Pos (.*), (.*), Ghost2Pos (.*), (.*), Ghost3Pos (.*), (.*),', line)
-		r = re.findall(r'\d+(?:,\d+)?', previous)#re.match(r'PacmanPos, (.*), (.*), Ghost0Pos (.*), (.*), Ghost1Pos (.*), (.*), Ghost2Pos (.*), (.*), Ghost3Pos (.*), (.*),', previous)
-		m = map(int, m)
-		r = map(int, r)
-		if("End of game" in line):
-			print('End of game', counter)
+        m = re.findall(r'\d+(?:,\d+)?', line)#re.match(r'PacmanPos, (.*), (.*), Ghost0Pos (.*), (.*), Ghost1Pos (.*), (.*), Ghost2Pos (.*), (.*), Ghost3Pos (.*), (.*),', line)
+        r = re.findall(r'\d+(?:,\d+)?', previous)#re.match(r'PacmanPos, (.*), (.*), Ghost0Pos (.*), (.*), Ghost1Pos (.*), (.*), Ghost2Pos (.*), (.*), Ghost3Pos (.*), (.*),', previous)
+        m = map(int, m)
+        r = map(int, r)
+        if("End of game" in line):
+            training_counter = training_counter + 1
+        
+            if(training_counter > TRAINING_SESSIONS):
+                for j in range (0, NUMBER_OF_GHOSTS):
+                    total = ArrayWithGhostCounters[j][0] + ArrayWithGhostCounters[j][1] + ArrayWithGhostCounters[j][2] + ArrayWithGhostCounters[j][3]
+                    
+                    PercentagesOfGames[counter][j] = ArrayWithGhostCounters[j]
+                    for k in range (0, 4):
+                        PercentagesOfGames[counter][j][k] = ArrayWithGhostCounters[j][k]/float(total)
+                        print(PercentagesOfGames[counter][j][k])
+                        
+                        #print('here')
 
-			
+                        ArrayWithGhostCounters[j][k] = 0;
+                    for k in range (0, 4):
+                        print('first',PercentagesOfGames[counter][j][k], ArrayWithGhostCounters[j][k]/float(total))
 
-			for j in range (0, NUMBER_OF_GHOSTS):
-				total = ArrayWithGhostCounters[j][0] + ArrayWithGhostCounters[j][1] + ArrayWithGhostCounters[j][2] + ArrayWithGhostCounters[j][3]
-				
-				PercentagesOfGames[counter][j] = ArrayWithGhostCounters[j]
-				for k in range (0, 4):
-					PercentagesOfGames[counter][j][k] = ArrayWithGhostCounters[j][k]/float(total)
-					print(PercentagesOfGames[counter][j][k])
-					
-					#print('here')
+                counter = counter + 1
 
-					ArrayWithGhostCounters[j][k] = 0;
-				for k in range (0, 4):
-					print('first',PercentagesOfGames[counter][j][k], ArrayWithGhostCounters[j][k]/float(total))
-				#print(PercentagesOfGames[counter][j][k], PercentagesOfGames[counter][j], np.sum(PercentagesOfGames[counter][j]	))
-			counter = counter + 1
+        #print ("previous %s \n current %s" % (previous, line))
+        if(training_counter > TRAINING_SESSIONS):
+            if((len(r) and len(m) >0 and len(m) == len(r)) and (len(m) >= 0 )):
+                
+                '''Ghost0Pos'''
+                #print(m[0], r[0] )
 
-		#print ("previous %s \n current %s" % (previous, line))
-		
-		if( (len(r)>0 and len(m) >0 and len(m) == len(r)) ):
-			
-			'''Ghost0Pos'''
-			#print(m[0], r[0] )
+                result = True;
+                for i in range(0, len(m)-1):
+                        result = ((m[i] -r[i] >= -1) and (m[i] -r[i]<= 1) ) and result
+                        
+                if(result):
 
-			result = True;
-			for i in range(0, len(m)-1):
-					result = ((m[i] -r[i] >= -1) and (m[i] -r[i]<= 1) ) and result
-					
+                    for i in range (0, NUMBER_OF_GHOSTS):
+                        #print(i, i*2, len(m), len(r))
+                        if(m[i*2] != r[i*2]):
+                            if((m[i*2] - r[i*2]) == 1):
+                                #0 is right
+                                ArrayWithGhostCounters[i][0] = ArrayWithGhostCounters[i][0] +1
+                                #rightG0 = rightG0 + 1
+                            elif((m[i*2] - r[i*2]) == -1):
+                                #1 is left
+                                ArrayWithGhostCounters[i][1] = ArrayWithGhostCounters[i][1] +1
+                                #leftG0 = leftG0 + 1
 
-			if(result):
+                        if(m[i*2 -1] != r[i*2 -1]):
+                            if((m[i*2 -1] - r[i*2 -1]) == 1):
+                                #2 is up
+                                ArrayWithGhostCounters[i][2] = ArrayWithGhostCounters[i][2] +1
+                                #upG0 = upG0 + 1
+                            elif((m[i*2 -1] - r[i*2 -1]) == -1):
+                                #3 is down
+                                ArrayWithGhostCounters[i][3] = ArrayWithGhostCounters[i][3] +1
+                                #downG0 = downG0 + 1
 
-				for i in range (0, NUMBER_OF_GHOSTS):
-					#print(i, i*2, len(m), len(r))
-					if(m[i*2] != r[i*2]):
-						if((m[i*2] - r[i*2]) == 1):
-							#0 is right
-							ArrayWithGhostCounters[i][0] = ArrayWithGhostCounters[i][0] +1
-							#rightG0 = rightG0 + 1
-						elif((m[i*2] - r[i*2]) == -1):
-							#1 is left
-							ArrayWithGhostCounters[i][1] = ArrayWithGhostCounters[i][1] +1
-							#leftG0 = leftG0 + 1
-
-					if(m[i*2 -1] != r[i*2 -1]):
-						if((m[i*2 -1] - r[i*2 -1]) == 1):
-							#2 is up
-							ArrayWithGhostCounters[i][2] = ArrayWithGhostCounters[i][2] +1
-							#upG0 = upG0 + 1
-						elif((m[i*2 -1] - r[i*2 -1]) == -1):
-							#3 is down
-							ArrayWithGhostCounters[i][3] = ArrayWithGhostCounters[i][3] +1
-							#downG0 = downG0 + 1
-
-
-
-
-
-				
-
-	previous = line;
+    previous = line;
 	#print ("previous %s \n current %s" % (previous, line))
 	
    	#print line,
@@ -112,22 +126,22 @@ allPositionsFromGhostsLeft = np.zeros(shape=(4,211))
 allPositionsFromGhostsRight = np.zeros(shape=(4,211))
 print('counter', counter)
 for i in range (0, NUMBER_OF_GAMES):
-	for j in range (0, NUMBER_OF_GHOSTS):
+    for j in range (0, NUMBER_OF_GHOSTS):
 		# for k in range (0, 4):
 		# 	print(i, j, k, PercentagesOfGames[i][j][k])
-		allPositionsFromGhostsUp[j][i] = PercentagesOfGames[i][j][2]
-		allPositionsFromGhostsDown[j][i] = PercentagesOfGames[i][j][3]
-		allPositionsFromGhostsLeft[j][i] = PercentagesOfGames[i][j][0]
-		allPositionsFromGhostsRight[j][i] = PercentagesOfGames[i][j][1]
+        allPositionsFromGhostsUp[j][i] = PercentagesOfGames[i][j][2]
+        allPositionsFromGhostsDown[j][i] = PercentagesOfGames[i][j][3]
+        allPositionsFromGhostsLeft[j][i] = PercentagesOfGames[i][j][0]
+        allPositionsFromGhostsRight[j][i] = PercentagesOfGames[i][j][1]
 #fcounter = float(counter);
 
 print("recorded")
 for i in range (0, NUMBER_OF_GAMES):
-	for j in range (0, NUMBER_OF_GHOSTS):
-		print (i, j, allPositionsFromGhostsUp[j][i])
-		print (i, j, allPositionsFromGhostsDown[j][i])
-		print (i, j, allPositionsFromGhostsLeft[j][i])
-		print (i, j, allPositionsFromGhostsRight[j][i])
+    for j in range (0, NUMBER_OF_GHOSTS):
+        print (i, j, allPositionsFromGhostsUp[j][i])
+        print (i, j, allPositionsFromGhostsDown[j][i])
+        print (i, j, allPositionsFromGhostsLeft[j][i])
+        print (i, j, allPositionsFromGhostsRight[j][i])
 
 
 total = 0.0;
@@ -136,24 +150,24 @@ averagesGhostsDown = np.zeros(4, dtype=float)
 averagesGhostsLeft = np.zeros(4, dtype=float)
 averagesGhostsRight = np.zeros(4, dtype=float)
 for j in range (0, NUMBER_OF_GHOSTS):
-	print (j, 'up', np.sum(allPositionsFromGhostsUp[j]))
-	print (j, 'down', np.sum(allPositionsFromGhostsDown[j]))
-	print (j, 'left',np.sum(allPositionsFromGhostsLeft[j]))
-	print (j, 'right', np.sum(allPositionsFromGhostsRight[j]))
-	total = float(np.sum(allPositionsFromGhostsUp[j]) + np.sum(allPositionsFromGhostsDown[j])+np.sum(allPositionsFromGhostsLeft[j])+np.sum(allPositionsFromGhostsRight[j]))
-	averagesGhostsUp[j] = np.sum(allPositionsFromGhostsUp[j])/float(total)
-	averagesGhostsDown[j] = np.sum(allPositionsFromGhostsDown[j])/float(total)
-	averagesGhostsLeft[j] = np.sum(allPositionsFromGhostsLeft[j])/float(total)
-	averagesGhostsRight[j] =  np.sum(allPositionsFromGhostsRight[j])/float(total)
-	print('total', total)
+    print (j, 'up', np.sum(allPositionsFromGhostsUp[j]))
+    print (j, 'down', np.sum(allPositionsFromGhostsDown[j]))
+    print (j, 'left',np.sum(allPositionsFromGhostsLeft[j]))
+    print (j, 'right', np.sum(allPositionsFromGhostsRight[j]))
+    total = float(np.sum(allPositionsFromGhostsUp[j]) + np.sum(allPositionsFromGhostsDown[j])+np.sum(allPositionsFromGhostsLeft[j])+np.sum(allPositionsFromGhostsRight[j]))
+    averagesGhostsUp[j] = np.sum(allPositionsFromGhostsUp[j])/float(total)
+    averagesGhostsDown[j] = np.sum(allPositionsFromGhostsDown[j])/float(total)
+    averagesGhostsLeft[j] = np.sum(allPositionsFromGhostsLeft[j])/float(total)
+    averagesGhostsRight[j] =  np.sum(allPositionsFromGhostsRight[j])/float(total)
+    print('total', total)
 
 print("finishedrecorded")
 
 for j in range (0, NUMBER_OF_GHOSTS):
-	print (j, averagesGhostsUp[j])
-	print (j, averagesGhostsDown[j])
-	print (j, averagesGhostsLeft[j])
-	print (j, averagesGhostsRight[j])
+    print (j, averagesGhostsUp[j])
+    print (j, averagesGhostsDown[j])
+    print (j, averagesGhostsLeft[j])
+    print (j, averagesGhostsRight[j])
 
 for j in range (0, NUMBER_OF_GHOSTS):
 	print("Ghost[%d] up: %f | down: %f | left: %f | right: %f" %(j, averagesGhostsUp[j], averagesGhostsDown[j],averagesGhostsLeft[j],averagesGhostsRight[j]))
