acfba2117771337a7b199447299bccfcfd336c9e
Alan R <alanribeiroandrade@gmail.com>
2018-06-04 17:35:05 +0200

    Added changes in mdp modeling

--- p3/extract_ghost_movements_percentage.py
+++ p3/extract_ghost_movements_percentage.py
@@ -9,12 +9,30 @@ import numpy as np
 # m = re.findall(r'\d', file)
 # print(m[0])
 
+pacMap = np.array(
+
+['%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%'],
+['%','.','.','.','.','.','.','%',' ',' ',' ',' ','%','.','.','.','.','.','.','%'],
+['%','.','%','%','.','.','.','%','%',' ',' ','%','%','.','.','.','%','%','.','%'],
+['%','.','%','.','.','%','.','.','.','.','.','.','.','.','%','.','.','%','.','%'],
+['%','.','%','%','.','%','.','%','%','%','%','%','%','.','%','.','%','%','.','%'],
+['%','.','.','.','.','.','.','.','.',' ','.','.','.','.','.','.','.','.','.','%'],
+['%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%','%'])
+
+yMap = pacMac.shape[0]
+xMap = pacMac.shape[1]
+
+ARRAY_FIELD_SIZE_X = ((xMap-2)*2)-1
+ARRAY_FIELD_SIZE_Y = ((yMap-2)*2)-1
+
+
+NUM_GHOSTS = 2
+
+outcomeRules = ''
+
 NUMBER_OF_GHOSTS = 2;
 NUMBER_OF_GAMES = 500;
 TRAINING_SESSIONS = 480;
-FIELD_OF_VIEW = 17;
-
-ARRAY_FIELD_SIZE = FIELD_OF_VIEW*2 +1
 POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD = 4;
 
 filename = "ghost_movements.csv"
@@ -23,9 +41,9 @@ previous = ""
 
 #ArrayWithGhostCounters = np.zeros(shape = (NUMBER_OF_GHOSTS,4))
 
-NEWArrayWithGhostCounters = np.zeros(shape=(ARRAY_FIELD_SIZE,ARRAY_FIELD_SIZE, NUMBER_OF_GHOSTS,POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD))
+NEWArrayWithGhostCounters = np.zeros(shape=(ARRAY_FIELD_SIZE_X,ARRAY_FIELD_SIZE_Y, NUMBER_OF_GHOSTS,POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD))
 
-PercentagesOfGames = np.zeros(shape=(NUMBER_OF_GAMES - TRAINING_SESSIONS, ARRAY_FIELD_SIZE,ARRAY_FIELD_SIZE, NUMBER_OF_GHOSTS,POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD))
+PercentagesOfGames = np.zeros(shape=(NUMBER_OF_GAMES - TRAINING_SESSIONS, ARRAY_FIELD_SIZE_X,ARRAY_FIELD_SIZE_Y, NUMBER_OF_GHOSTS,POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD))
 
 counter = 0
 training_counter = 0;
@@ -50,8 +68,8 @@ for line in file:
         
             if(training_counter > TRAINING_SESSIONS):
                 for j in range (0, NUMBER_OF_GHOSTS):
-                    for p in range(0, ARRAY_FIELD_SIZE):
-                        for q in range(0,ARRAY_FIELD_SIZE):
+                    for p in range(0, ARRAY_FIELD_SIZE_X):
+                        for q in range(0,ARRAY_FIELD_SIZE_Y):
                         
                             total = NEWArrayWithGhostCounters[p][q][j][0] + NEWArrayWithGhostCounters[p][q][j][1] + NEWArrayWithGhostCounters[p][q][j][2] + NEWArrayWithGhostCounters[p][q][j][3]
 
@@ -87,10 +105,10 @@ for line in file:
                 if(result):
                     for i in range (1, NUMBER_OF_GHOSTS+1):
                         
-                        locX = r[0] - r[i*2] + FIELD_OF_VIEW;
-                        locY = r[1] - r[1 + i*2] + FIELD_OF_VIEW;
+                        locX = r[0] - r[i*2] + xMap;
+                        locY = r[1] - r[1 + i*2] + yMap;
                         #print(locX,'(',r[0], r[i*2],')', locY, '(', r[1], r[1 + i*2], ')')
-                        if(((locX < ARRAY_FIELD_SIZE and locX >= 0) and (locY < ARRAY_FIELD_SIZE and locY >= 0))):
+                        if(((locX < ARRAY_FIELD_SIZE_X and locX >= 0) and (locY < ARRAY_FIELD_SIZE_Y and locY >= 0))):
 
                             #print(locX,'(',r[0], r[i*2],')', locY, '(', r[1], r[1 + i*2], ')')
                             if(m[i*2] != r[i*2]):
@@ -122,15 +140,15 @@ for line in file:
     previous = line;
 	
    	#print line,
-allPositionsFromGhostsUp = np.zeros(shape=(ARRAY_FIELD_SIZE,ARRAY_FIELD_SIZE,4,NUMBER_OF_GAMES - TRAINING_SESSIONS))
-allPositionsFromGhostsDown = np.zeros(shape=(ARRAY_FIELD_SIZE,ARRAY_FIELD_SIZE, 4,NUMBER_OF_GAMES - TRAINING_SESSIONS))
-allPositionsFromGhostsLeft = np.zeros(shape=(ARRAY_FIELD_SIZE, ARRAY_FIELD_SIZE,4,NUMBER_OF_GAMES - TRAINING_SESSIONS))
-allPositionsFromGhostsRight = np.zeros(shape=(ARRAY_FIELD_SIZE, ARRAY_FIELD_SIZE,4,NUMBER_OF_GAMES - TRAINING_SESSIONS))
+allPositionsFromGhostsUp = np.zeros(shape=(ARRAY_FIELD_SIZE_X,ARRAY_FIELD_SIZE_Y,NUMBER_OF_GHOSTS,NUMBER_OF_GAMES - TRAINING_SESSIONS))
+allPositionsFromGhostsDown = np.zeros(shape=(ARRAY_FIELD_SIZE_X,ARRAY_FIELD_SIZE_Y, NUMBER_OF_GHOSTS,NUMBER_OF_GAMES - TRAINING_SESSIONS))
+allPositionsFromGhostsLeft = np.zeros(shape=(ARRAY_FIELD_SIZE_X, ARRAY_FIELD_SIZE_Y,NUMBER_OF_GHOSTS,NUMBER_OF_GAMES - TRAINING_SESSIONS))
+allPositionsFromGhostsRight = np.zeros(shape=(ARRAY_FIELD_SIZE_X, ARRAY_FIELD_SIZE_Y,NUMBER_OF_GHOSTS,NUMBER_OF_GAMES - TRAINING_SESSIONS))
 #print('counter', counter)
 for i in range (0, NUMBER_OF_GAMES - TRAINING_SESSIONS):
     for j in range (0, NUMBER_OF_GHOSTS):
-        for p in range(0, ARRAY_FIELD_SIZE):
-            for q in range(0,ARRAY_FIELD_SIZE):
+        for p in range(0, ARRAY_FIELD_SIZE_X):
+            for q in range(0,ARRAY_FIELD_SIZE_Y):
                 allPositionsFromGhostsUp[p][q][j][i] = PercentagesOfGames[i][p][q][j][2]
                 allPositionsFromGhostsDown[p][q][j][i] = PercentagesOfGames[i][p][q][j][3]
                 allPositionsFromGhostsLeft[p][q][j][i] = PercentagesOfGames[i][p][q][j][0]
@@ -150,10 +168,10 @@ for i in range (0, NUMBER_OF_GAMES - TRAINING_SESSIONS):
 '''
 
 total = 0.0;
-averagesGhosts = np.zeros(shape=(POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD, ARRAY_FIELD_SIZE, ARRAY_FIELD_SIZE, NUMBER_OF_GHOSTS), dtype=float)
+averagesGhosts = np.zeros(shape=(POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD, ARRAY_FIELD_SIZE_X, ARRAY_FIELD_SIZE_Y, NUMBER_OF_GHOSTS), dtype=float)
 for j in range (0, NUMBER_OF_GHOSTS):
-    for p in range(0, ARRAY_FIELD_SIZE):
-            for q in range(0,ARRAY_FIELD_SIZE):
+    for p in range(0, ARRAY_FIELD_SIZE_X):
+            for q in range(0,ARRAY_FIELD_SIZE_Y):
                 # print (j, 'up', np.sum(allPositionsFromGhostsUp[p][q][j]))
                 # print (j, 'down', np.sum(allPositionsFromGhostsDown[p][q][j]))
                 # print (j, 'left',np.sum(allPositionsFromGhostsLeft[p][q][j]))
@@ -161,7 +179,7 @@ for j in range (0, NUMBER_OF_GHOSTS):
                 total = float(np.sum(allPositionsFromGhostsUp[p][q][j]) + np.sum(allPositionsFromGhostsDown[p][q][j])+np.sum(allPositionsFromGhostsLeft[p][q][j])+np.sum(allPositionsFromGhostsRight[p][q][j]))
                 if(np.sum(allPositionsFromGhostsUp[p][q][j]) > 0):
                     averagesGhosts[0][p][q][j] = np.sum(allPositionsFromGhostsUp[p][q][j])/float(total)
-                if(np.sum(allPositionsFromGhostsDown[p][q][j]) > 0):
+                if(np.sum(allPositionsFromGhostsDown[p][q][j])   > 0):
                     averagesGhosts[1][p][q][j] = np.sum(allPositionsFromGhostsDown[p][q][j])/float(total)
                 if(np.sum(allPositionsFromGhostsLeft[p][q][j]) > 0):
                     averagesGhosts[2][p][q][j] = np.sum(allPositionsFromGhostsLeft[p][q][j])/float(total)
@@ -170,15 +188,15 @@ for j in range (0, NUMBER_OF_GHOSTS):
                 # print('total', total)
 
 # print("finishedrecorded")
-'''
+
 for j in range (0, NUMBER_OF_GHOSTS):
-    for p in range(0, ARRAY_FIELD_SIZE):
-            for q in range(0,ARRAY_FIELD_SIZE):
-                print ('up', 'ghost', j, p - FIELD_OF_VIEW, q- FIELD_OF_VIEW, averagesGhostsUp[p][q][j])
-                print ('down', 'ghost',j, p- FIELD_OF_VIEW, q- FIELD_OF_VIEW, averagesGhostsDown[p][q][j])
-                print ('left', 'ghost',j, p- FIELD_OF_VIEW, q- FIELD_OF_VIEW, averagesGhostsLeft[p][q][j])
-                print ('right', 'ghost',j, p- FIELD_OF_VIEW, q- FIELD_OF_VIEW, averagesGhostsRight[p][q][j])
-'''
+    for p in range(0, ARRAY_FIELD_SIZE_X):
+            for q in range(0,ARRAY_FIELD_SIZE_Y):
+                print ('up', 'ghost', j, p - xMap, q- yMap, averagesGhosts[0][p][q][j])
+                print ('down', 'ghost',j, p- xMap, q- yMap, averagesGhosts[1][p][q][j])
+                print ('left', 'ghost',j, p- xMap, q- yMap, averagesGhosts[2][p][q][j])
+                print ('right', 'ghost',j, p- xMap, q- yMap, averagesGhosts[3][p][q][j])
+
 
 def getIdPartly(currentValueX, currentValueY):
     result = ''
@@ -242,23 +260,28 @@ def getGhostMovement(pacmanAction, ghostAction, ghostx, ghosty):
             return (ghostx - 2), ghosty
         if(ghostAction == 3): #right
             return ghostx, ghosty
+
         
         
 print('mdp')
 print('')
+print('const xSize ='+ xMap+';')
+print('const ySize ='+ yMap+';')
+print('')
 print('module pacman')
 print('')
-print('\ts : [0..', end='')
-for j in range (0, NUMBER_OF_GHOSTS*2): # maximum state is the maximum coordinates for x and y for each ghost
-    print(ARRAY_FIELD_SIZE-1, end='')
-
-print('];')    
+print('\txG0 : [0..xSize] init 8; // x position of Ghost')
+print('\tyG0 : [0..ySize] init 1; // y position of Ghost')
+print('\txG1 : [0..xSize] init 11; // x position of Ghost')
+print('\tyG1 : [0..ySize] init 1; // y position of Ghost')
 print('')
 
-for ghost1X in range(0, ARRAY_FIELD_SIZE):
-    for ghost1Y in range(0, ARRAY_FIELD_SIZE):
-        for ghost2X in range(0, ARRAY_FIELD_SIZE):
-            for ghost2Y in range(0, ARRAY_FIELD_SIZE):
+
+
+for ghost1X in range(0, ARRAY_FIELD_SIZE_X):
+    for ghost1Y in range(0, ARRAY_FIELD_SIZE_Y):
+        for ghost2X in range(0, ARRAY_FIELD_SIZE_X):
+            for ghost2Y in range(0, ARRAY_FIELD_SIZE_Y):
                 for action in range(0, POSSIBLE_MOVEMENTS_IN_A_2DIMENSIONAL_WORLD):
                     first_part = '[' + str(action) + '] s='
                     first_part += getId(ghost1X, ghost1Y, ghost2X, ghost2Y)
@@ -284,3 +307,20 @@ print("endmodule")
 			
 
 
+
+for g in range(0, NUM_GHOSTS):
+    for x in range(0, xMap):
+        for y in range(0, yMap):
+            if(pacMap[x][y] == ' ' || pacMap[x][y] == '.'):
+                #left
+                if(pacMap[x-1][y] != '%'):
+                    outcomeRules += '[g'+str(g)+ '] (y ='+str(y) + '& x'+ str(x)+') -> 1:(x\' = x-1)' +'\n'
+                #right    
+                if(pacMap[x+1][y] != '%'):
+                    outcomeRules += '[g'+str(g)+ '] (y ='+str(y) + '& x'+ str(x)+') -> 1:(x\' = x+1)' +'\n'
+                #up
+                if(pacMap[x][y-1] != '%'):
+                    outcomeRules += '[g'+str(g)+ '] (y ='+str(y) + '& x'+ str(x)+') -> 1:(y\' = y-1)' +'\n'
+
+                if(pacMap[x][y+1] != '%'):
+                    outcomeRules += '[g'+str(g)+ '] (y ='+str(y) + '& x'+ str(x)+') -> 1:(y\' = y+1)' +'\n'
